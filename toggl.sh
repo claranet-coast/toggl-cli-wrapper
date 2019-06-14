9e0202a716b79e68822b725ba2bc1db4887ef09d
Angelo Gelmini <angelogelmini@Air-di-Angelo.homenet.telecomitalia.it>
2019-06-14 12:11:56 +0200

    Added functions

--- toggl.sh
+++ toggl.sh
@@ -77,8 +77,8 @@ function range()
 				weekend_check=`gdate -d $(($index-$INDEX_CURRENT_DAY))-days +%u`
 				if [ "$weekend_check" -le "5" ]; then
 					DATE=`gdate -d $(($index-$INDEX_CURRENT_DAY))-days +%Y-%m-%d`
-					#toggl add "$DATE 9:00AM" "$DATE 1:00PM" "mattina"
-					#toggl add "$DATE 2:00PM" "$DATE 6:00PM" "pomeriggio"
+					toggl add "$DATE 9:00AM" "$DATE 1:00PM" "mattina"
+					toggl add "$DATE 2:00PM" "$DATE 6:00PM" "pomeriggio"
 				fi
 			done
 		else
